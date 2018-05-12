057c858a00a38089e77a5782226fb37f9d496766
Kashish <kashishgoyal2019@u.northwestern.edu>
2018-05-12 13:16:33 -0500

    Final running code

--- request.py
+++ request.py
@@ -56,7 +56,7 @@ def display(image, window_name = 'img', delay = 0):
     cv2.namedWindow(window_name,cv2.WINDOW_NORMAL)
     cv2.imshow(window_name,image)
     cv2.waitKey(delay)
-    # cv2.destroyAllWindows()
+    cv2.destroyAllWindows()
 
 def get_tiles(latitude1,longitude1,latitude2, longitude2, levelOfDetail):
     a,b = latlon2pix(latitude1,longitude1,levelOfDetail)
@@ -64,7 +64,6 @@ def get_tiles(latitude1,longitude1,latitude2, longitude2, levelOfDetail):
     a,b = latlon2pix(latitude2,longitude2,levelOfDetail)
     x2,y2 = pixel2tile(a,b)
     return x1,y1,x2,y2
-
 
 
 
@@ -79,34 +78,36 @@ def main():
     b = a.find('ImageUrl').contents[0]
     '''
 
-    lat1 = 48.0
-    lon1 = 52.0
-    lat2 = 49.0
-    lon2 = 53.0
-    levelOfDetail = 8
+    # lat1 = 48.0
+    # lon1 = 52.0
+    # lat2 = 49.0
+    # lon2 = 53.0
+    # levelOfDetail = 8
 
-    # lat1 = input ('Enter latitude1: ')
-    # lon1 = input ('Enter longitude1: ')
-    # lat2 = input ('Enter latitude2: ')
-    # lon2 = input ('Enter longitude2: ')
-    # levelOfDetail = input('Enter level of detail: ')
+    lat1 = input ('Enter latitude1: ')
+    lon1 = input ('Enter longitude1: ')
+    lat2 = input ('Enter latitude2: ')
+    lon2 = input ('Enter longitude2: ')
+    levelOfDetail = input('Enter level of detail: ')
 
 
     tileX1, tileY1, tileX2, tileY2 = get_tiles(lat1, lon1, lat2, lon2, levelOfDetail)
-    print tileX1, tileY1, tileX2, tileY2
+    print "Total number of tiles: ", abs((tileX1 -tileX2)*(tileY1 - tileY2))
+    print 'Getting images ...'
     for i in range(min(tileX1,tileX2),max(tileX1,tileX2)+1,1):
         for j in range(min(tileY1,tileY2),max(tileY1,tileY2)+1,1):
             qk = quad(i,j,levelOfDetail)
             url  = 'http://ecn.t0.tiles.virtualearth.net/tiles/h' + qk +'.jpeg?g=131'
             r = requests.get(url, stream = True)
-            print r.url
+            # print r.url
             img_ny = get_image(r)
             if (j==min(tileY1,tileY2)): img_py = img_ny
             else: img_py = np.concatenate((img_py, img_ny), axis=0)
             # display(img_py)
         if (i==min(tileX1,tileX2)): img_px = img_py
         else: img_px = np.concatenate((img_px, img_py), axis=1)
-        display(img_px)
+    display(img_px)
+    cv2.imwrite('output.png', img_px)
 
 
 if __name__ == '__main__':
